8f09c833ac91088cca77b2ce0fb68c02a1e3b515
Saad <66124515+sadsultan@users.noreply.github.com>
2023-06-26 13:45:52 +0500

    fixed typeError in the beats object and bug in computer selection

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let choices = ["rock", "paper", "scissors"];
 let beats = {
-    rock: "scissors",
-    paper: "rock",
-    scissors: "paper"
-}
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper"
+};
 
 function computerPlay() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
+    return choices[Math.floor(Math.random() * 3)];
+};
 
 
 function playRound(playerSelection, computerSelection) {
@@ -18,8 +18,10 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return "You lose!";
     }
-}
+};
 
-let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+let playerSelection = "rock";// prompt("Rock, paper, or scissors?").toLowerCase();
 let computerSelection = computerPlay();
+console.log("Player: " + playerSelection);
+console.log("Computer: " + computerSelection);
 console.log(playRound(playerSelection, computerSelection));
