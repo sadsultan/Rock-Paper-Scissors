31eb06b79a9e2cba743869526822c544eb385bd4
Saad <66124515+sadsultan@users.noreply.github.com>
2023-06-26 13:35:40 +0500

    Added playRound function

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ let beats = {
 function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie!";
+    } else if (beats[computerSelection] == playerSelection) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
