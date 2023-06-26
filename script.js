2e4a48b181548246df0e8164d804aeab5fc691dc
Saad <66124515+sadsultan@users.noreply.github.com>
2023-06-26 13:56:15 +0500

    Placed main body inside an unbound while loop

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerSelection;
+let computerSelection;
 let choices = ["rock", "paper", "scissors"];
 let beats = {
     "rock": "scissors",
@@ -14,14 +16,16 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return "Tie!";
     } else if (beats[computerSelection] == playerSelection) {
-        return "You lose!";
+        return "You lose the round!";
     } else {
-        return "You win!";
+        return "You win this round!";
     }
 };
 
-let playerSelection = "rock";// prompt("Rock, paper, or scissors?").toLowerCase();
-let computerSelection = computerPlay();
-console.log("Player: " + playerSelection);
-console.log("Computer: " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+while (1) {
+    playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+    computerSelection = computerPlay();
+    console.log("Player: " + playerSelection);
+    console.log("Computer: " + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+}
